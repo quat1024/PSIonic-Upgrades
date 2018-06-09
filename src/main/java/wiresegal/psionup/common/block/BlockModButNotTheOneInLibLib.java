c8f876f67253f27b522673a73773d57b2cf8fc5b
quat <scratch933@gmail.com>
2018-06-09 11:19:26 -0400

    improve blockmod

--- src/main/java/wiresegal/psionup/common/block/BlockModButNotTheOneInLibLib.java
+++ src/main/java/wiresegal/psionup/common/block/BlockModButNotTheOneInLibLib.java
@@ -1,12 +1,17 @@
 package wiresegal.psionup.common.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import wiresegal.psionup.common.lib.LibMisc;
 
 public class BlockModButNotTheOneInLibLib extends Block {
 	public BlockModButNotTheOneInLibLib(String name, Material mat) {
-		super(mat);
+		this(name, mat, mat.getMaterialMapColor());
+	}
+	
+	public BlockModButNotTheOneInLibLib(String name, Material mat, MapColor color) {
+		super(mat, color);
 		
 		setRegistryName(LibMisc.MOD_ID + ":" + name);
 		setUnlocalizedName(LibMisc.MOD_ID + "." + name);
