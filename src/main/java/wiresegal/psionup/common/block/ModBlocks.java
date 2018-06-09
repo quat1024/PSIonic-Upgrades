6267ac984b52800dfc6c271ccb524217e161d9fd
quat <scratch933@gmail.com>
2018-06-09 02:30:14 -0400

    lazily add a million objectholders, if i might need them in the future

--- src/main/java/wiresegal/psionup/common/block/ModBlocks.java
+++ src/main/java/wiresegal/psionup/common/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package wiresegal.psionup.common.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 import wiresegal.psionup.common.block.spell.BlockConjuredPulsar;
@@ -10,6 +11,22 @@ import wiresegal.psionup.common.lib.LibMisc;
 import wiresegal.psionup.common.lib.LibNames;
 
 public class ModBlocks {
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Blocks.CONJURED_PULSAR)
+	public static final Block conjuredPulsar = Blocks.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Blocks.CONJURED_STAR)
+	public static final Block conjuredStar = Blocks.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Blocks.BRIGHT_PLATE)
+	public static final Block brightPlate = Blocks.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Blocks.DARK_PLATE)
+	public static final Block darkPlate = Blocks.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Blocks.CAD_CASE)
+	public static final Block cadCase = Blocks.AIR;
+	
 	public static void register(IForgeRegistry<Block> reg) {
 		reg.register(new BlockConjuredPulsar(LibNames.Blocks.CONJURED_PULSAR));
 		reg.register(new BlockConjuredStar(LibNames.Blocks.CONJURED_STAR));
--- src/main/java/wiresegal/psionup/common/items/ModItems.java
+++ src/main/java/wiresegal/psionup/common/items/ModItems.java
@@ -1,14 +1,102 @@
 package wiresegal.psionup.common.items;
 
 import com.teamwizardry.librarianlib.features.base.item.ItemMod;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 import wiresegal.psionup.common.items.component.*;
 import wiresegal.psionup.common.items.spell.*;
+import wiresegal.psionup.common.lib.LibMisc;
 import wiresegal.psionup.common.lib.LibNames;
 
 public class ModItems {
+	
+	//CADs and CAD Accessories
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.LIQUID_INK_COLORIZER)
+	public static final Item liquidColorizer = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.DRAINED_COLORIZER)
+	public static final Item drainedColorizer = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.INLINE_CASTER)
+	public static final Item inlineCaster = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.SPELL_MAGAZINE)
+	public static final Item spellMagazine = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.WIDE_BAND_SOCKET)
+	public static final Item wideBandSocket = Items.AIR;
+	
+	//Ebony and Ivory Tools and Armor
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_HELMET)
+	public static final Item ebonyHelmet = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_CHEST)
+	public static final Item ebonyChest = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_LEGS)
+	public static final Item ebonyLegs = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_BOOTS)
+	public static final Item ebonyBoots = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_HELMET)
+	public static final Item ivoryHelmet = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_CHEST)
+	public static final Item ivoryChest = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_LEGS)
+	public static final Item ivoryLegs = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_BOOTS)
+	public static final Item ivoryBoots = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_PICKAXE)
+	public static final Item ebonyPickaxe = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_SHOVEL)
+	public static final Item ebonyShovel = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_AXE)
+	public static final Item ebonyAxe = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.EBONY_SWORD)
+	public static final Item ebonySword = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_PICKAXE)
+	public static final Item ivoryPickaxe = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_SHOVEL)
+	public static final Item ivoryShovel = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_AXE)
+	public static final Item ivoryAxe = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.IVORY_SWORD)
+	public static final Item ivorySword = Items.AIR;
+	
+	//Other Stuff
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.FLASH_RING)
+	public static final Item flashRing = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.BIOTIC_SENSOR)
+	public static final Item bioticSensor = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.GAUSS_RIFLE)
+	public static final Item gaussRifle = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.GAUSS_BULLET)
+	public static final Item gaussBullet = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.UNSTABLE_BATTERY)
+	public static final Item unstableBattery = Items.AIR;
+	
+	@GameRegistry.ObjectHolder(LibMisc.MOD_ID + ":" + LibNames.Items.TWINFLOW_BATTERY)
+	public static final Item twinflowBattery = Items.AIR;
+	
 	public static void register(IForgeRegistry<Item> reg) {
 		reg.register(new ItemLiquidColorizer(LibNames.Items.LIQUID_INK_COLORIZER));
 		reg.register(new ItemEmptyColorizer(LibNames.Items.DRAINED_COLORIZER));
