5e3e32331bdf1d62ed96ca80a275cead114cbd61
quat <scratch933@gmail.com>
2018-06-25 23:43:11 -0400

    Add condition factories for use later

---
+++ src/main/java/wiresegal/psionup/common/crafting/factory/SpellMagazineConditionFactory.java
@@ -0,0 +1,17 @@
+package wiresegal.psionup.common.crafting.factory;
+
+import com.google.gson.JsonObject;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.IConditionFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+import wiresegal.psionup.common.core.ConfigHandler;
+
+import java.util.function.BooleanSupplier;
+
+public class SpellMagazineConditionFactory implements IConditionFactory {
+	@Override
+	public BooleanSupplier parse(JsonContext context, JsonObject json) {
+		boolean value = JsonUtils.getBoolean(json, "value", true);
+		return () -> ConfigHandler.enableMagazine == value;
+	}
+}
